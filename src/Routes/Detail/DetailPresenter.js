8f1a11d87dcb15722d73349df99ae24bba7a8b80
ghehd231 <ghehd231@naver.com>
2019-08-09 20:28:12 +0900

    Presenter structer #2

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,2 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-export default () => "Detail";
+
+
+const DetailPresenter = ({
+    result,
+    loading,
+    error
+    
+}) => null;
+
+DetailPresenter.propTypes = {
+    result: PropTypes.array,
+    error : PropTypes.array,
+    loading: PropTypes.bool.isRequired
+};
+
+export default DetailPresenter;

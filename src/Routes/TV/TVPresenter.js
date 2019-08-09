24921ba57be4d465369c8adccec0a70b42917f22
ghehd231 <ghehd231@naver.com>
2019-08-09 21:02:28 +0900

    TVPresenter and LoaderComponent

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
-const TVPresenter = ({topRated, popular, airing_today, loading, error}) => null;
+
+const Container = styled.div`
+    padding: 0px 10px;
+`;
+
+
+const TVPresenter = ({topRated, popular, airing_today, loading, error}) => (
+    loading ? <Loader /> : (
+        <Container>
+             {topRated && topRated.length > 0 && (
+            <Section title = "topRated">
+                {topRated.map(tv => (
+                    <span>{tv.name}></span>
+                ))}
+            </Section>
+            )}
+            {popular && popular.length > 0 && (
+            <Section title = "popular">
+                {popular.map(tv => (
+                    <span>{tv.name}></span>
+                ))}
+            </Section>
+            )}
+             {airing_today && airing_today.length > 0 && (
+            <Section title = "airing_today">
+                {airing_today.map(tv => (
+                    <span>{tv.name}></span>
+                ))}
+            </Section>
+            )}
+        </Container>
+    )
+);
 
 TVPresenter.propTypes = {
     topRated: PropTypes.array,
@@ -13,3 +47,27 @@ TVPresenter.propTypes = {
 };
 
 export default TVPresenter;
+
+// loading ? <Loader/> : (
+//     <Container>
+//         {nowPlaying && nowPlaying.length > 0 && (
+//             <Section title = "Now Playing">
+//                 {nowPlaying.map(movie => (
+//                     <span key={movie.id}>{movie.title}></span>
+//                 ))}
+//             </Section>
+//         )}
+//         {upcoming && upcoming.length > 0 && (
+//             <Section title = "Upcomming">
+//                 {upcoming.map(movie => (    
+//                     <span key={movie.id}>{movie.title}</span>
+//                 ))}
+//             </Section>
+//         )}
+//         {popular && popular.length > 0 && (
+//             <Section title = "popular">
+//                 {popular.map(movie => movie.title)}
+//             </Section>
+//         )}
+//     </Container>
+// );
